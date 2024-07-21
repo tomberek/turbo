a570c707025a9ef69a2f9f10f7f78e55522ad46c
Tom Bereknyei <tomberek@gmail.com>
2024-07-21 06:36:50 -0400

    chore: format flake

--- flake.nix
+++ flake.nix
@@ -7,42 +7,45 @@
     packages = builtins.mapAttrs (system: pkgs: rec {
 
       # grab the recipe for tracelinks directly
-      tracelinks = pkgs.callPackage (_.tracelinks + "/pkgs/tracelinks") {self=_.tracelinks;};
+      tracelinks = pkgs.callPackage (_.tracelinks + "/pkgs/tracelinks") { self = _.tracelinks; };
 
-      turbo = pkgs.runCommandNoCC "g" {
-        buildInputs = [
-          pkgs.docopts
-          pkgs.findutils.locate
-          pkgs.gum
-          pkgs.gnused
-          pkgs.nix-index
-          tracelinks
-        ];
-        nativeBuildInputs = [
-          pkgs.makeWrapper
-          pkgs.shfmt
-          pkgs.shellcheck
-        ];
-      } ''
-        mkdir -p $out/bin
-        substitute ${./g.sh} $out/bin/g \
-          --subst-var-by locate ${pkgs.findutils.locate}/bin/locate \
-          --subst-var-by updatedb ${pkgs.findutils.locate}/bin/updatedb \
-          --subst-var-by nix-locate ${pkgs.nix-index}/bin/nix-locate \
-          --subst-var-by tracelinks ${tracelinks}/bin/tracelinks \
-          --subst-var-by docopts ${pkgs.docopts}/bin/docopts \
-          --subst-var-by sed ${pkgs.gnused}/bin/sed \
-          --subst-var-by gum ${pkgs.gum}/bin/gum
-        chmod +x $out/bin/g
-        patchShebangs $out/bin/g
-        ln -s $out/bin/g $out/bin/turbo
-        ln -s $out/bin/g $out/bin/321
-        ln -s $out/bin/g $out/bin/'$'
-        set -eu
-        shfmt -s -d -i 4 $out/bin/g
-        shellcheck $out/bin/g
-        $out/bin/g --version
-      '';
+      turbo =
+        pkgs.runCommandNoCC "g"
+          {
+            buildInputs = [
+              pkgs.docopts
+              pkgs.findutils.locate
+              pkgs.gum
+              pkgs.gnused
+              pkgs.nix-index
+              tracelinks
+            ];
+            nativeBuildInputs = [
+              pkgs.makeWrapper
+              pkgs.shfmt
+              pkgs.shellcheck
+            ];
+          }
+          ''
+            mkdir -p $out/bin
+            substitute ${./g.sh} $out/bin/g \
+              --subst-var-by locate ${pkgs.findutils.locate}/bin/locate \
+              --subst-var-by updatedb ${pkgs.findutils.locate}/bin/updatedb \
+              --subst-var-by nix-locate ${pkgs.nix-index}/bin/nix-locate \
+              --subst-var-by tracelinks ${tracelinks}/bin/tracelinks \
+              --subst-var-by docopts ${pkgs.docopts}/bin/docopts \
+              --subst-var-by sed ${pkgs.gnused}/bin/sed \
+              --subst-var-by gum ${pkgs.gum}/bin/gum
+            chmod +x $out/bin/g
+            patchShebangs $out/bin/g
+            ln -s $out/bin/g $out/bin/turbo
+            ln -s $out/bin/g $out/bin/321
+            ln -s $out/bin/g $out/bin/'$'
+            set -eu
+            shfmt -s -d -i 4 $out/bin/g
+            shellcheck $out/bin/g
+            $out/bin/g --version
+          '';
       default = turbo;
     }) _.nixpkgs.legacyPackages;
   };
